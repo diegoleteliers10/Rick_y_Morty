9d4ad7d3cc93124c78057d22cbc3c83629a8971a
diegoleteliers10 <diegoleteliers10@gmail.com>
2023-04-18 18:03:16 -0400

    ready

---
+++ Client/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Cards from './components/Cards/Cards';
+import axios from 'axios';
+import Nav from './components/Nav/Nav';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import About from './components/About/About'
+import Detail from './components/Detail'
+import Form from './components/Form/Form'
+import { useLocation } from "react-router-dom";
+import Favorites from './components/Favorites/Favorites';
+ 
+
+function App() {
+
+   const navigate= useNavigate()
+   let [access,setAccess]= useState(false)
+   const email= "diegoleteliers10@gmail.com"
+   const password= "Sofi2009"
+
+   function login(userData){
+      if(userData.password===password && userData.email===email){
+         setAccess(true)
+         navigate('/home')
+      }
+   }
+
+   function logout(userData){
+      setAccess(false)
+      navigate('/')
+   }
+
+   useEffect(()=>{
+      if(access===false){
+         navigate('/')
+      }
+   },[access, navigate])
+
+   const [characters, setCharacters]= React.useState([])
+
+   const onSearch= (id)=>{
+      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+      if (data.name) {
+         setCharacters((oldChars) => [...oldChars, data]);
+      } else {
+         window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose=(id)=>{
+      const distintos = characters.filter(personaje=> personaje.id !== id);
+      setCharacters(distintos)
+   }
+    let location = useLocation()
+    let isLocation= location.pathname==='/'
+
+   return (
+
+
+      <div className='App'>
+
+         {isLocation? 
+            <Form prop={login}/> : <Nav characters={characters} onSearch= {onSearch} logOut={logout} />
+         }
+            <div className='container'>
+               <Routes>
+                  <Route path='/home' element={<Cards characters={characters} Close={onClose}/>} />
+                  <Route path='/about' element={<About/>}/>
+                  <Route path='/detail/:id' element={<Detail id={characters}/>}/>
+                  <Route path='/favorite' element={<Favorites Close={onClose}/>}/>
+               </Routes>
+            </div>
+         
+      </div>
+   );
+}
+
+export default App;
---
+++ Server/src/index.js
@@ -0,0 +1,23 @@
+const http= require('http')
+const data= require('./utils/data');
+
+http.createServer((req,res)=>{
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log('Server is up in: 3001')
+
+  if(req.url.includes('/rickandmorty/character')){
+
+    const id= parseInt(req.url.split('/').pop())
+    console.log(id)
+    const character= data.find(c=>c.id===id)
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(character))
+
+  }else {
+
+    res.writeHead(404, {"Content-Type":"text/plain"})
+    return res.write("No se encontro el personaje")
+
+  }
+}).listen(3001,'localhost')
