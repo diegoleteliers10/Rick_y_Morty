451b7b8ebbd29edbe51bf79d00a69353d5421eaa
diegoleteliers10 <diegoleteliers10@gmail.com>
2023-04-19 15:51:34 -0400

    ready

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -2,6 +2,7 @@ const axios= require('axios')
 
 getCharById= (res,id)=>{
   axios(`https://rickandmortyapi.com/api/character/${id}`)
+  
   .then(response=>{
     let char= {
       id: id,
@@ -15,6 +16,7 @@ getCharById= (res,id)=>{
     res.writeHead(200,{'Content-Type':'application/json'})
     res.end(JSON.stringify(char))
   })
+
   .catch(error=>{
     res.writeHead(500,{'Content-Type':'text/plain'})
     res.end(error.message)
